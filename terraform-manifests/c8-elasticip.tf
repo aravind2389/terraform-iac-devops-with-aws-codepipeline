52651abf5609dc29dff23641f56f46caa9e31e4c
aravind <aravindkundurthi98@gmail.com>
2021-10-14 19:18:27 +0530

    local-exec removed

--- terraform-manifests/c8-elasticip.tf
+++ terraform-manifests/c8-elasticip.tf
@@ -4,10 +4,11 @@ resource "aws_eip" "bastion_eip" {
   instance                  = module.ec2_public.id[0]
   vpc                       = true
   tags = local.common_tags
-  provisioner "local-exec" {
+  /*provisioner "local-exec" {
   command = "echo Destroy time prov `date` >> destroy-time-prov.txt"
   working_dir = "local-exec-output-files/"
   when = destroy
   #on_failure = continue
   }
+  */
 }
