e36012fdfdf0dd1037ab1cf38f7d306a4d62a751
aravind <aravindkundurthi98@gmail.com>
2021-10-14 18:39:02 +0530

    Fifth commit

--- terraform-manifests/c10-01-ALB-application-loadbalancer-variables.tf
+++ terraform-manifests/c10-01-ALB-application-loadbalancer-variables.tf
@@ -1,6 +1,6 @@
 # Terraform AWS Application Load Balancer Variables
 # Place holder file for AWS ALB Variables
-
+/*
 # App1 DNS Name
 variable "app1_dns_name" {
   description = "App1 DNS Name"
@@ -10,3 +10,5 @@ variable "app1_dns_name" {
 variable "app2_dns_name" {
   description = "App2 DNS Name"
 }
+
+*/
